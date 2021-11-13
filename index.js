70fe6e24bb728059f0d9e544f867c2bbc4208cc3
gilmachadoB <gilderlanemachado96@gmail.com>
2021-11-12 22:11:31 -0300

    recriando landpage

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function myFunction(ev) {
   const nome = document.getElementById("nome").value;
-  const sobrenome = document.getElementById("sobrenome").value;
+  const telefone = document.getElementById("telefone").value;
   const email = document.getElementById("email").value;
   const emails = localStorage.getItem("emails");
   if (emails) {
     const store = JSON.parse(emails);
-    store.push({ nome, sobrenome, email });
+    store.push({ nome, telefone, email });
     localStorage.setItem("emails", JSON.stringify(store));
     alert("Dados salvos com sucesso!");
     return;
   }
-  localStorage.setItem("emails", JSON.stringify([{ nome, sobrenome, email }]));
+  localStorage.setItem("emails", JSON.stringify([{ nome, telefone, email }]));
   alert("Dados salvos com sucesso!");
 }
